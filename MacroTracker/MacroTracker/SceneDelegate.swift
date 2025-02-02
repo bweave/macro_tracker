2837881a91cce80dee73a530190fbbd4bf9ecb9e
Brian Weaver <bdrums@gmail.com>
2025-02-02 16:02:41 -0500

    Fix some leftover spaces from formatting

--- MacroTracker/MacroTracker/SceneDelegate.swift
+++ MacroTracker/MacroTracker/SceneDelegate.swift
@@ -3,17 +3,17 @@ import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
   var window: UIWindow?
-  
+
   private var coordinator: TabCoordinator?
-  
+
   func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
     guard let windowScene = scene as? UIWindowScene else { return }
-    
+
     UINavigationBar.configureWithOpaqueBackground()
-    
+
     let window = UIWindow(windowScene: windowScene)
     self.window = window
-    
+
     coordinator = TabCoordinator(window: window)
     coordinator?.start()
   }
