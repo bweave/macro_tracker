c4b69fbf5c19fd0398158445b4a5db283df8f367
Brian Weaver <bdrums@gmail.com>
2025-02-14 11:22:12 -0500

    Fix login bug for iOS
    Logging out and back in was weird. You'd end up on the Stats page on the
    Account tab. `refresh_or_redirect_to` fixes things.

--- macro_tracker_rails/app/controllers/sessions_controller.rb
+++ macro_tracker_rails/app/controllers/sessions_controller.rb
@@ -11,14 +11,9 @@ class SessionsController < ApplicationController
   def create
     if user = User.authenticate_by(params.permit(:email_address, :password))
       start_new_session_for user
-      if user.goals.exists?
-        redirect_to after_authentication_url
-      else
-        redirect_to account_path
-      end
+      refresh_or_redirect_to after_authentication_url
     else
-      redirect_to new_session_path,
-                  alert: "Try another email address or password."
+      redirect_to new_session_path, alert: "Try another email address or password."
     end
   end
 
